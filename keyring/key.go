45e91aa7323345349bf3f492838feed5e4b8ef10
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-05-07 17:53:49 +0200

    more proto to amino migration in keyring pkg

--- keyring/key.go
+++ keyring/key.go
@@ -1,6 +1,11 @@
 package keyring
 
 import (
+	"fmt"
+
+	"github.com/tbruyelle/legacykey/codec"
+
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	cosmoskeyring "github.com/cosmos/cosmos-sdk/crypto/keyring"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 )
@@ -29,4 +34,87 @@ func (k Key) GetPrivKey() (cryptotypes.PrivKey, error) {
 
 func (k Key) IsAmino() bool {
 	return k.Info != nil
+}
+
+func (k Key) ToProto() ([]byte, error) {
+	// Turn record to legacyInfo
+	info, err := legacyInfoFromRecord(k.Record)
+	if err != nil {
+		return nil, err
+	}
+	return codec.Amino.MarshalLengthPrefixed(info)
+}
+
+func extractPrivKeyFromLocal(rl *cosmoskeyring.Record_Local) (cryptotypes.PrivKey, error) {
+	if rl.PrivKey == nil {
+		return nil, cosmoskeyring.ErrPrivKeyNotAvailable
+	}
+
+	priv, ok := rl.PrivKey.GetCachedValue().(cryptotypes.PrivKey)
+	if !ok {
+		return nil, cosmoskeyring.ErrCastAny
+	}
+
+	return priv, nil
+}
+
+func privKeyFromRecord(record *cosmoskeyring.Record) (cryptotypes.PrivKey, error) {
+	switch record.GetType() {
+	case cosmoskeyring.TypeLocal:
+		return extractPrivKeyFromLocal(record.GetLocal())
+	}
+	return nil, fmt.Errorf("unhandled Record type %q", record.GetType())
+}
+
+func privKeyFromInfo(info cosmoskeyring.LegacyInfo) (privKey cryptotypes.PrivKey, err error) {
+	switch info.GetType() {
+	case cosmoskeyring.TypeLocal:
+		err = codec.Amino.Unmarshal([]byte(info.(legacyLocalInfo).GetPrivKeyArmor()), &privKey)
+		return
+	}
+	return nil, fmt.Errorf("unhandled Info type %q", info.GetType())
+}
+
+// legacyInfoFromLegacyInfo turns a Record into a LegacyInfo.
+func legacyInfoFromRecord(record *cosmoskeyring.Record) (cosmoskeyring.LegacyInfo, error) {
+	switch record.GetType() {
+	case cosmoskeyring.TypeLocal:
+		pk, err := record.GetPubKey()
+		if err != nil {
+			return nil, err
+		}
+		privKey, err := extractPrivKeyFromLocal(record.GetLocal())
+		if err != nil {
+			return nil, err
+		}
+		privBz, err := codec.Amino.Marshal(privKey)
+		if err != nil {
+			return nil, err
+		}
+		return legacyLocalInfo{
+			Name:         record.Name,
+			PubKey:       pk,
+			Algo:         hd.PubKeyType(pk.Type()),
+			PrivKeyArmor: string(privBz),
+		}, nil
+
+	case cosmoskeyring.TypeLedger:
+		pk, err := record.GetPubKey()
+		if err != nil {
+			return nil, err
+		}
+		return legacyLedgerInfo{
+			Name:   record.Name,
+			PubKey: pk,
+			Algo:   hd.PubKeyType(pk.Type()),
+			Path:   *record.GetLedger().Path,
+		}, nil
+
+	case cosmoskeyring.TypeMulti:
+		panic("record type TypeMulti unhandled")
+
+	case cosmoskeyring.TypeOffline:
+		panic("record type TypeOffline unhandled")
+	}
+	panic(fmt.Sprintf("record type %s unhandled", record.GetType()))
 }
--- migrate_keys.go
+++ migrate_keys.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/tbruyelle/legacykey/codec"
 	"github.com/tbruyelle/legacykey/keyring"
 )
 
@@ -33,16 +32,11 @@ func migrateKeys(keyringDir string) error {
 		}
 		// this is a proto-encoded key let's migrate it back to amino
 		fmt.Printf("%q (proto encoded)-> %s\n", key.Name, spew.Sdump(key.Record))
-		// Turn record to legacyInfo
-		info, err := keyring.LegacyInfoFromRecord(key.Record)
+		bz, err := key.ToProto()
 		if err != nil {
 			return err
 		}
 		// Register new amino key_name.info -> amino encoded LegacyInfo
-		bz, err := codec.Amino.MarshalLengthPrefixed(info)
-		if err != nil {
-			return err
-		}
 		if err := aminoKr.Set(key.Name, bz); err != nil {
 			return err
 		}
