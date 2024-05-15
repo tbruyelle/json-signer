66f984dfd04cd2b44bdf269265171746c68b4d63
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-05-15 16:53:21 +0200

    refactor: move ledger code to specific file

--- keyring/key.go
+++ keyring/key.go
@@ -2,17 +2,13 @@ package keyring
 
 import (
 	"fmt"
-	"math/big"
 
 	"github.com/tbruyelle/amino-signer/codec"
 
-	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/btcsuite/btcd/btcec/v2/ecdsa"
 	ledger "github.com/cosmos/ledger-cosmos-go"
 
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	cosmoskeyring "github.com/cosmos/cosmos-sdk/crypto/keyring"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 )
 
@@ -22,10 +18,6 @@ type Key struct {
 	record *cosmoskeyring.Record
 	// info is not nil if the key is amino-encoded
 	info cosmoskeyring.LegacyInfo
-}
-
-func discoverLedger() (*ledger.LedgerCosmos, error) {
-	return ledger.FindLedgerCosmosUserApp()
 }
 
 func (k Key) Name() string {
@@ -61,27 +53,11 @@ func (k Key) Sign(bz []byte) ([]byte, cryptotypes.PubKey, error) {
 		return signature, privKey.PubKey(), nil
 
 	case cosmoskeyring.TypeLedger:
-		device, err := discoverLedger()
+		device, err := ledger.FindLedgerCosmosUserApp()
 		if err != nil {
 			return nil, nil, err
 		}
-		path, err := k.getBip44Path()
-		if err != nil {
-			return nil, nil, err
-		}
-		pubKey, err := getLedgerPubKey(device, path.DerivationPath())
-		if err != nil {
-			return nil, nil, err
-		}
-		signature, err := device.SignSECP256K1(path.DerivationPath(), bz, 0)
-		if err != nil {
-			return nil, nil, err
-		}
-		signature, err = convertDERtoBER(signature)
-		if err != nil {
-			return nil, nil, err
-		}
-		return signature, pubKey, nil
+		return signWithLedger(device, k, bz)
 	}
 	return nil, nil, fmt.Errorf("unhandled key type %q", k.GetType())
 }
@@ -108,51 +84,6 @@ func (k Key) getPrivKey() (cryptotypes.PrivKey, error) {
 	}
 	// Get priv key from proto encoded key
 	return extractPrivKeyFromLocal(k.record.GetLocal())
-}
-
-func getLedgerPubKey(device *ledger.LedgerCosmos, bip32Path []uint32) (cryptotypes.PubKey, error) {
-	pubKey, err := device.GetPublicKeySECP256K1(bip32Path)
-	if err != nil {
-		return nil, err
-	}
-	// re-serialize in the 33-byte compressed format
-	cmp, err := btcec.ParsePubKey(pubKey)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing public key: %v", err)
-	}
-
-	compressedPublicKey := make([]byte, secp256k1.PubKeySize)
-	copy(compressedPublicKey, cmp.SerializeCompressed())
-
-	return &secp256k1.PubKey{Key: compressedPublicKey}, nil
-}
-
-func convertDERtoBER(signatureDER []byte) ([]byte, error) {
-	sigDER, err := ecdsa.ParseDERSignature(signatureDER)
-	if err != nil {
-		return nil, err
-	}
-
-	sigStr := sigDER.Serialize()
-	// The format of a DER encoded signature is as follows:
-	// 0x30 <total length> 0x02 <length of R> <R> 0x02 <length of S> <S>
-	r, s := new(big.Int), new(big.Int)
-	r.SetBytes(sigStr[4 : 4+sigStr[3]])
-	s.SetBytes(sigStr[4+sigStr[3]+2:])
-
-	sModNScalar := new(btcec.ModNScalar)
-	sModNScalar.SetByteSlice(s.Bytes())
-	// based on https://github.com/tendermint/btcd/blob/ec996c5/btcec/signature.go#L33-L50
-	if sModNScalar.IsOverHalfOrder() {
-		s = new(big.Int).Sub(btcec.S256().N, s)
-	}
-
-	sigBytes := make([]byte, 64)
-	// 0 pad the byte arrays from the left if they aren't big enough.
-	copy(sigBytes[32-len(r.Bytes()):32], r.Bytes())
-	copy(sigBytes[64-len(s.Bytes()):64], s.Bytes())
-
-	return sigBytes, nil
 }
 
 func extractPrivKeyFromLocal(rl *cosmoskeyring.Record_Local) (cryptotypes.PrivKey, error) {
