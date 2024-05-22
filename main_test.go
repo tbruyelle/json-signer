0b4e73d7c7781bd97c2ff01fd4058c85788279e9
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-05-22 16:17:19 +0200

    replace txtar custom cmd with env

--- main_test.go
+++ main_test.go
@@ -10,14 +10,19 @@ import (
 	"github.com/tbruyelle/keyring-compat"
 )
 
-func TestScripts(t *testing.T) {
+func TestE2E(t *testing.T) {
+	var (
+		dir           = t.TempDir()
+		gaiadBin      = filepath.Join(dir, "gaiad")
+		jsonSignerBin = filepath.Join(dir, "json-signer")
+	)
 	// Build json-signer bin
-	err := exec.Command("go", "build", "-o=/tmp/json-signer", ".").Run()
+	err := exec.Command("go", "build", "-o", jsonSignerBin, ".").Run()
 	if err != nil {
 		t.Fatalf("can't build json-signer: %v", err)
 	}
 	// Build gaiad bin
-	err = exec.Command("go", "build", "-o=/tmp/gaiad",
+	err = exec.Command("go", "build", "-o", gaiadBin,
 		"-modfile=testdeps/go.mod",
 		"github.com/cosmos/gaia/v15/cmd/gaiad",
 	).Run()
@@ -28,17 +33,11 @@ func TestScripts(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Dir:      "testdata",
 		TestWork: true,
-		/*
-			Setup: func(env *testscript.Env) error {
-				if err := runGaiad(env.Cd, "init", "gaia-test"); err != nil {
-					return err
-				}
-				if err := runGaiad(env.Cd, strings.Fields("keys add test1 --keyring-backend=test")...); err != nil {
-					return err
-				}
-				return nil
-			},
-		*/
+		Setup: func(env *testscript.Env) error {
+			env.Setenv("GAIAD", gaiadBin)
+			env.Setenv("JSONSIGNER", jsonSignerBin)
+			return nil
+		},
 		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
 			"fillenvs": func(ts *testscript.TestScript, neg bool, args []string) {
 				// Fill $TEST1 with bech32 address
@@ -60,31 +59,6 @@ func TestScripts(t *testing.T) {
 				}
 				ts.Setenv("TEST1", addr)
 			},
-			"gaiad": func(ts *testscript.TestScript, neg bool, args []string) {
-				tsExec(ts, neg, "/tmp/gaiad", args)
-			},
-			"json-signer": func(ts *testscript.TestScript, neg bool, args []string) {
-				tsExec(ts, neg, "/tmp/json-signer", args)
-			},
 		},
 	})
-}
-
-func tsExec(ts *testscript.TestScript, neg bool, cmd string, args []string) {
-	err := ts.Exec(cmd, args...)
-	if err != nil {
-		ts.Logf("%s command error: %+v", cmd, err)
-	}
-
-	commandSucceeded := (err == nil)
-	successExpected := !neg
-
-	// Compare the command's success status with the expected outcome.
-	if commandSucceeded != successExpected {
-		ts.Fatalf("unexpected %s command outcome (err=%t expected=%t)", cmd, commandSucceeded, successExpected)
-	}
-	if err != nil {
-		// TODO handle neg param
-		ts.Fatalf(err.Error())
-	}
 }
